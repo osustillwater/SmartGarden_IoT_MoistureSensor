cc82ece792c8a6d89d44aa1ef76866de0afd2f6c
saif <osustillwater@gmail.com>
2020-01-03 14:02:34 +0000

    moisture

--- moistureSensor/moistureSensor.ino
+++ moistureSensor/moistureSensor.ino
@@ -12,15 +12,10 @@ StaticJsonDocument<200> doc;
 int max_humidity = 0; //wet
 int min_humidity = 0; //dry
 
-//int max_humidity = 700; //wet
-//int min_humidity = 1023; //dry
-
 //WiFi
 char* ssid = "labs";
 char* password = "robot1cA!ESTG";
 
-//char* ssid = "eventos";
-//char* password = "2019";
 
 //API
 String resourceGetThreshold = "http://smart-garden-api.azurewebsites.net/api/Moisture/threshold";
@@ -38,25 +33,17 @@ void setup() {
   
   Serial.begin(115200);
   
- //+++++ Initialize WiFi +++++
- 
-  Serial.print(F("Initializing Wi-Fi "));
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(F("."));
-  }
-  Serial.println(F("OK."));
-  //-----
-
-  //++++ Show wifi configs +++++
+     Serial.print(F("Initializing Wi-Fi "));
+     WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(F("."));
+       }
+       Serial.println(F("OK."));
   
   Serial.println(F("--- Configuracoes Wi-Fi ---"));
   EITIWiFi.showWifiSettings();
   Serial.println(F("-----"));
-  
-  //-----
-
   Serial.println();
 }
 
@@ -72,25 +59,10 @@ void loop()
         Serial.println("Error converting to percent") ;
         return ;
       }
-   // Serial.print(percent);
-   // Serial.println("%");
-
-//*************************************************
-//   HTTP: Perform the request
-//*************************************************
-
-
-    //get current temperature from API
-
-      //ToDO: Research how to Get this data (tansform void to function to get the json)
-      
-     // max_humidity = EITIWiFi.httpGet(resourceGetThreshold).toFloat();
-     // min_humidity = EITIWiFi.httpGet(resourceGetConfiguration).toFloat();
-     // Serial.println("Threshold");
-     
+   
       Serial.println(jsonGetMoistureThreshold);
 
-      String data = "{ \"Percentage\": " + String(percent) + "}" ;
+      String data = "{ \"value\": " + String(percent) + "}" ;
       Serial.print(data);
       Serial.println("%");
       EITIWiFi.httpPost(resourcePost, EITIWifi_POST_JSON, data);
@@ -104,7 +76,6 @@ void loop()
       WiFi.reconnect();
       Serial.println(F("-----"));
     }
-    //-----
     
    delay(1000);
   
@@ -112,11 +83,11 @@ void loop()
 
 int convertToPercent(int value)
 {
-  int percentValue = 0;
+   int percentValue = 0;
    ConfigurationValue = EITIWiFi.httpGet(resourceGetConfiguration) ;
 
  // Deserialize the JSON document
-  DeserializationError error = deserializeJson(doc, ConfigurationValue);
+    DeserializationError error = deserializeJson(doc, ConfigurationValue);
 
   // Test if parsing succeeds.
   if (error) {
@@ -126,9 +97,6 @@ int convertToPercent(int value)
   }
 
   // Fetch values.
-  //
-  // Most of the time, you can rely on the implicit casts.
-  // In other case, you can do doc["time"].as<long>();
   
    min_humidity  = doc["minValue"];
    max_humidity = doc["maxValue"];
@@ -136,12 +104,3 @@ int convertToPercent(int value)
    percentValue = map(value, min_humidity, max_humidity, 0, 100);
    return (percentValue);
 }
-
-// the loop routine runs over and over again forever:
-/*void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(1000);        // delay in between reads for stability
-} */
